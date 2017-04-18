34302850c80428ea2fd1c78c9af8446e399752fc
joplevy <joey.plevy@gmail.com>
2017-04-19 01:38:29 +0200

    ok

--- champs/ex.s
+++ champs/ex.s
@@ -1,7 +1,9 @@
 .name "zork"
 .comment "just a basic living prog"
 		
-l2:	sti	r1,%:live,%0
-	and	r1,%0,r1
-live:	live	%1
-	zjmp	%:live
+l2:		sti		r1, %:live1, %1
+		sti		r1, %:live2, %1
+		sti		r1, %:live3, %1
+live1:	live	%1
+live2:	live	%1
+live3:	live	%1
